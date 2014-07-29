ee0c873997bf98965098b2fc6cab29ddc6ce54f9
Arnaud Pflieger <arnaud.pflieger@lesfurets.com>
2014-07-30 00:51:56 +0200

    Added a test in failure mecanism to catch docker's container exit code

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,3 +1,19 @@
-docker build -t "$1" $1
-docker run -i --rm $1
-docker rmi -f $1
+docker build -t "$1" $1 1> /dev/null
+echo "============================="
+echo "TEST OUTPUT"
+echo "============================="
+docker run --cidfile="$1_cid" -i $1
+echo "============================="
+cid=`cat $1_cid`
+exit=`docker wait "$cid"`
+docker rm "$cid" 1> /dev/null
+rm "$1_cid"
+docker rmi -f $1 1> /dev/null
+
+if [ $exit == 0 ]
+then
+	echo "SUCCESS"
+else
+	echo "FAILED"
+fi
+
