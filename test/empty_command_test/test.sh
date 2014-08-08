a9359e6b051aaf9863680058d3a1be5fd077238e
Arnaud Pflieger <arnaud.pflieger@lesfurets.com>
2014-08-08 15:09:52 +0200

    Fix behaviour when there is no arguments given Added test case

---
+++ test/empty_command_test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#FIXME
+git status &> /dev/null
+
+#saving the initial state of the repository
+sha1=`git rev-parse HEAD`
+
+/home/git-octopus
+
+if [ $? -ne 0 ] ; then
+	exit 1
+fi
+
+if [[ `git rev-parse HEAD` != $sha1 ]] ; then
+	echo "should stayed at HEAD"
+	exit 1
+fi
+
+if [[ -n `git diff-index HEAD` ]] ; then
+	echo "repository should be clean"
+	exit 1
+fi
+
+if [[ `git symbolic-ref HEAD` != "refs/heads/master" ]] ; then
+	echo "Should be still on master"
+	exit 1
+fi
