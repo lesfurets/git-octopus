0e396945d9971859925a306bc8e1c99763154337
Arnaud Pflieger <arnaud.pflieger@lesfurets.com>
2014-10-28 14:40:37 +0100

    Added config support for octopus.commit (true|false) default to true false has the same behaviour than git-octopus -n The option -n overrides the config

---
+++ test/no_commit_config_test/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#FIXME
+git status &> /dev/null
+
+git config octopus.commit false
+#saving the initial state of the repository
+sha1=`git rev-parse HEAD`
+
+/home/git-octopus features/*
+
+#should be back to HEAD
+if [[ `git rev-parse HEAD` != $sha1 ]] ; then
+    exit 1
+fi
+
+#repository should be clean
+if [[ -n `git diff-index HEAD` ]] ; then
+    exit 1
+fi
+
+#should be still on master
+if [[ `git symbolic-ref HEAD` != "refs/heads/master" ]] ; then
+    echo "Should be still on master"
+    exit 1
+fi
