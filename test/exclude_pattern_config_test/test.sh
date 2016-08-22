f57290741bf45d215cece29c2d855b0f137bd690
Ozan Gunalp <ozangunalp@gmail.com>
2016-08-22 17:01:28 +0200

    exclude branches from octopus with -e pattern

---
+++ test/exclude_pattern_config_test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#FIXME
+git status &> /dev/null
+
+git checkout features/feat1
+git config octopus.pattern features/*
+git config --add octopus.pattern master
+
+git config octopus.excludePattern "features/feat3"
+
+git octopus
+merged=`git branch --merged`
+if [[ $merged != *feat1* ]] ; then
+	exit 1
+fi
+if [[ $merged != *feat2* ]] ; then
+	exit 1
+fi
+if [[ $merged == *feat3* ]] ; then
+	exit 1
+fi
+if [[ $merged != *master* ]] ; then
+	exit 1
+fi
