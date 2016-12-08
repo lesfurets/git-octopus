25a5748eff287e844093477019b4f5d20dcd0113
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-12-08 18:05:05 +0100

    No more pwd, repository is used everywhere

--- git-octopus.go
+++ git-octopus.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-	mainWithArgs(".", os.Args[1:])
+	repo := repository{ path: "."}
+	mainWithArgs(&repo, os.Args[1:])
 }
 
-func mainWithArgs(pwd string, args []string) {
+func mainWithArgs(repo *repository, args []string) {
 
-	octopusConfig := getOctopusConfig(pwd, args)
+	octopusConfig := getOctopusConfig(repo, args)
 
 	if octopusConfig.printVersion {
 		fmt.Println("2.0")
@@ -19,14 +20,14 @@ func mainWithArgs(pwd string, args []string) {
 	}
 }
 
-func resolveBranchList(pwd string, patterns []string, excludedPatterns []string) map[string]string {
-	result :=   parseLsRemote(git(pwd,  append([]string{"ls-remote", "."}, patterns...)...))
+func resolveBranchList(repo *repository, patterns []string, excludedPatterns []string) map[string]string {
+	result :=   parseLsRemote(repo.git(append([]string{"ls-remote", "."}, patterns...)...))
 
 	if len(excludedPatterns) == 0 {
 		return result
 	}
 	
-	excludedRefs := parseLsRemote(git(pwd,  append([]string{"ls-remote", "."}, excludedPatterns...)...))
+	excludedRefs := parseLsRemote(repo.git(append([]string{"ls-remote", "."}, excludedPatterns...)...))
 	for excludedRef, _ := range excludedRefs {
 		delete(result, excludedRef)
 	}
