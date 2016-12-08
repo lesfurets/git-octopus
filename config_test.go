36531130d39897403b73eb50d5ce1986f018a54d
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-12-08 16:31:54 +0100

    Added type that abstract the current git repository. Holds the path of the repo so we don't have to reference pwd everywhere in tests

--- config_test.go
+++ config_test.go
@@ -1,42 +1,39 @@
 package main
 
 import (
-	"io/ioutil"
 	"reflect"
 	"testing"
 )
 
 func TestDoCommit(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "git-octopus-test-")
-
-	git(dir, "init")
+	repo := createRepo()
 
 	// No config, no option. doCommit should be true
-	octopusConfig := getOctopusConfig(dir, nil)
+	octopusConfig := getOctopusConfig(repo.path, nil)
 
 	if !octopusConfig.doCommit {
 		t.Error("Expected doCommit = true, got", octopusConfig.doCommit)
 	}
 
 	// Config to false, no option. doCommit should be false
-	git(dir, "config", "octopus.commit", "false")
-	octopusConfig = getOctopusConfig(dir, nil)
+	repo.git("config", "octopus.commit", "false")
+	octopusConfig = getOctopusConfig(repo.path, nil)
 
 	if octopusConfig.doCommit {
 		t.Error("Expected doCommit = false, got", octopusConfig.doCommit)
 	}
 
 	// Config to false, -c option takes precedence. doCommit should be true
-	git(dir, "config", "octopus.commit", "false")
-	octopusConfig = getOctopusConfig(dir, []string{"-c"})
+	repo.git("config", "octopus.commit", "false")
+	octopusConfig = getOctopusConfig(repo.path, []string{"-c"})
 
 	if !octopusConfig.doCommit {
 		t.Error("Expected doCommit = true, got", octopusConfig.doCommit)
 	}
 
 	// Config to true, -n option takes precedence. doCommit should be false
-	git(dir, "config", "octopus.commit", "true")
-	octopusConfig = getOctopusConfig(dir, []string{"-n"})
+	repo.git("config", "octopus.commit", "true")
+	octopusConfig = getOctopusConfig(repo.path, []string{"-n"})
 
 	if octopusConfig.doCommit {
 		t.Error("Expected doCommit = false, got", octopusConfig.doCommit)
@@ -44,19 +41,17 @@ func TestDoCommit(t *testing.T) {
 }
 
 func TestChunkMode(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "git-octopus-test-")
-
-	git(dir, "init")
+	repo := createRepo()
 
 	// No option. chunkSize should be set to 0
-	octopusConfig := getOctopusConfig(dir, nil)
+	octopusConfig := getOctopusConfig(repo.path, nil)
 
 	if octopusConfig.chunkSize != 0 {
 		t.Error("Expected chunkSize = 0, got", octopusConfig.chunkSize)
 	}
 
 	// -s 5, chunkSize should be set to 5
-	octopusConfig = getOctopusConfig(dir, []string{"-s", "5"})
+	octopusConfig = getOctopusConfig(repo.path, []string{"-s", "5"})
 
 	if octopusConfig.chunkSize != 5 {
 		t.Error("Expected chunkSize = 5, got", octopusConfig.chunkSize)
@@ -64,28 +59,26 @@ func TestChunkMode(t *testing.T) {
 }
 
 func TestExcludedPatterns(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "git-octopus-test-")
-
-	git(dir, "init")
+	repo := createRepo()
 
 	// No config, no option. excludedPatterns should be empty
-	octopusConfig := getOctopusConfig(dir, nil)
+	octopusConfig := getOctopusConfig(repo.path, nil)
 
 	if len(octopusConfig.excludedPatterns) > 0 {
 		t.Error("Expected excludedPatterns to be empty")
 	}
 
 	// Config given, no option. excludedPatterns should be set
-	git(dir, "config", "octopus.excludePattern", "excluded/*")
-	git(dir, "config", "--add", "octopus.excludePattern", "excluded_branch")
-	octopusConfig = getOctopusConfig(dir, nil)
+	repo.git("config", "octopus.excludePattern", "excluded/*")
+	repo.git("config", "--add", "octopus.excludePattern", "excluded_branch")
+	octopusConfig = getOctopusConfig(repo.path, nil)
 
 	if !reflect.DeepEqual(octopusConfig.excludedPatterns, []string{"excluded/*", "excluded_branch"}) {
 		t.Error("actual excludedPatterns:", octopusConfig.excludedPatterns)
 	}
 
 	// Config given (from previous assertion), option given. Option should take precedence
-	octopusConfig = getOctopusConfig(dir, []string{"-e", "override_excluded"})
+	octopusConfig = getOctopusConfig(repo.path, []string{"-e", "override_excluded"})
 
 	if !reflect.DeepEqual(octopusConfig.excludedPatterns, []string{"override_excluded"}) {
 		t.Error("excludedPatterns", octopusConfig.excludedPatterns)
@@ -93,28 +86,26 @@ func TestExcludedPatterns(t *testing.T) {
 }
 
 func TestPatterns(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "git-octopus-test-")
-
-	git(dir, "init")
+	repo := createRepo()
 
 	// No config, no option. excludedPatterns should be empty
-	octopusConfig := getOctopusConfig(dir, nil)
+	octopusConfig := getOctopusConfig(repo.path, nil)
 
 	if len(octopusConfig.patterns) > 0 {
 		t.Error("Expected patterns to be empty")
 	}
 
 	// Config given, no argument. patterns should be set
-	git(dir, "config", "octopus.pattern", "test")
-	git(dir, "config", "--add", "octopus.pattern", "test2")
-	octopusConfig = getOctopusConfig(dir, nil)
+	repo.git("config", "octopus.pattern", "test")
+	repo.git("config", "--add", "octopus.pattern", "test2")
+	octopusConfig = getOctopusConfig(repo.path, nil)
 
 	if !reflect.DeepEqual(octopusConfig.patterns, []string{"test", "test2"}) {
 		t.Error("actual patterns:", octopusConfig.patterns)
 	}
 
 	// Config given (from previous assertion), argument given. Arguments should take precedence
-	octopusConfig = getOctopusConfig(dir, []string{"arg1", "arg2"})
+	octopusConfig = getOctopusConfig(repo.path, []string{"arg1", "arg2"})
 
 	if !reflect.DeepEqual(octopusConfig.patterns, []string{"arg1", "arg2"}) {
 		t.Error("actual patterns:", octopusConfig.patterns)
