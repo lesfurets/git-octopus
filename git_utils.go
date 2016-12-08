089c100bdb3140392c7c83a9c7ef0e3a5e5194c9
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-12-08 16:31:54 +0100

    Added type that abstract the current git repository. Holds the path of the repo so we don't have to reference pwd everywhere in tests

--- git_utils.go
+++ git_utils.go
@@ -3,10 +3,64 @@ package main
 import (
 	"os/exec"
 	"strings"
+	"bufio"
+	"path/filepath"
+	"runtime"
+	"io/ioutil"
+	"fmt"
 )
 
 func git(repoPath string, args ...string) string {
 	out, _ := exec.Command("git", append([]string{"-C", repoPath}, args...)...).Output()
 
 	return strings.TrimSpace(string(out[:]))
+}
+
+// Takes the output of git-ls-remote. Returns a map sha1 => refsname
+func parseLsRemote(lsRemoteOutput string) map[string]string {
+	result := map[string]string{}
+
+	if len(lsRemoteOutput) == 0 {
+		return result
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(lsRemoteOutput))
+
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), "\t")
+
+		result[split[0]] = split[1]
+	}
+
+	return result
+}
+
+
+type repository struct {
+	path string
+}
+
+func createRepo() *repository {
+	dir, _ := ioutil.TempDir("", "git-octopus-test-")
+
+	repository := repository{ path: dir}
+
+	git(dir, "init")
+
+	return &repository
+}
+
+func (repo *repository) git(args ...string) string {
+	return git(repo.path, args...)
+}
+
+func (repo *repository) run(script string) {
+	_, file, _, _ := runtime.Caller(0)
+	scriptPath := filepath.Join(filepath.Dir(file), script)
+	cmd := exec.Command(scriptPath)
+	cmd.Dir = repo.path
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
