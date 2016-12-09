6321a21ca52c9fdf2b89996bca31d20fbf5677be
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-12-09 13:06:43 +0100

    Handle some basic error cases Arguments passed to mainWithArgs are now variadic so it's more convenient in tests

--- git-octopus.go
+++ git-octopus.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	repo := repository{path: "."}
-	mainWithArgs(&repo, os.Args[1:])
+	mainWithArgs(&repo, os.Args[1:]...)
 }
 
-func mainWithArgs(repo *repository, args []string) {
+func mainWithArgs(repo *repository, args ...string) {
 
 	octopusConfig := getOctopusConfig(repo, args)
 
 	if octopusConfig.printVersion {
 		fmt.Println("2.0")
+		return
+	}
+
+	if len(octopusConfig.patterns) == 0 {
+		fmt.Println("Nothing to merge. No pattern given")
+		return
+	}
+
+	branchList := resolveBranchList(repo, octopusConfig.patterns, octopusConfig.excludedPatterns)
+
+	if len(branchList) == 0 {
+		fmt.Printf("No branch matching \"%v\" were found\n", strings.Join(octopusConfig.patterns, " "))
 		return
 	}
 }
