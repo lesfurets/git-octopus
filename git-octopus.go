446a1a6c55cb1f2ea73c18b32d2efedceca25600
hvi <hugo.vinet@lesfurets.com>
2016-12-10 13:44:23 +0100

    Extract pattern matching functions and tests to matcher.go

--- git-octopus.go
+++ git-octopus.go
@@ -32,18 +32,3 @@ func mainWithArgs(repo *repository, args ...string) {
 		return
 	}
 }
-
-func resolveBranchList(repo *repository, patterns []string, excludedPatterns []string) map[string]string {
-	result := parseLsRemote(repo.git(append([]string{"ls-remote", "."}, patterns...)...))
-
-	if len(excludedPatterns) == 0 {
-		return result
-	}
-
-	excludedRefs := parseLsRemote(repo.git(append([]string{"ls-remote", "."}, excludedPatterns...)...))
-	for excludedRef, _ := range excludedRefs {
-		delete(result, excludedRef)
-	}
-
-	return result
-}
