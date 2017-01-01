09c7f7a0595c932851f0ce0d8edfbc5f467674f7
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2017-01-01 16:06:48 +0100

    Reset the repo on interrupt or kill signals

--- git-octopus.go
+++ git-octopus.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lesfurets/git-octopus/run"
 	"log"
 	"os"
+	"os/signal"
 )
 
 func main() {
@@ -15,6 +16,11 @@ func main() {
 		Logger: log.New(os.Stdout, "", 0),
 	}
 
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, os.Kill)
+
+	go handleSignals(signalChan, &context)
+
 	err := run.Run(&context, os.Args[1:]...)
 
 	if err != nil {
@@ -23,4 +29,23 @@ func main() {
 		}
 		os.Exit(1)
 	}
+}
+
+func handleSignals(signalChan chan os.Signal, context *run.OctopusContext) {
+	initialHeadCommit, _ := context.Repo.Git("rev-parse", "HEAD")
+	/*
+	 The behavior of this is quite tricky. The signal is not only received on signalChan
+	 but sent to subprocesses started by exec.Command as well. It is likely that
+	 the main go routine is running one of those subprocess which will stop and return an error.
+	 The error is handled by the Run function as any other error depending on where the execution was.
+
+	 In the mean time, this routine is resetting the repo.
+
+	 This is definitly an approximation that works in most cases.
+	 */
+	signal := <-signalChan
+	context.Logger.Printf("Signal %v\n", signal.String())
+	context.Repo.Git("reset", "-q", "--hard", initialHeadCommit)
+	context.Repo.Git("clean", "-fd")
+	os.Exit(1)
 }
