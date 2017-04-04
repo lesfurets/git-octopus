43ccf763a8492dbaee51be1ebcd0ade340efd593
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2017-04-04 14:52:28 +0200

    Reproduce the usecase of #32 about duplicate parent error

--- run/basics_test.go
+++ run/basics_test.go
@@ -25,6 +25,12 @@ func writeFile(repo *git.Repository, name string, lines ...string) {
 	ioutil.WriteFile(fileName, []byte(strings.Join(lines, "\n")), 0644)
 }
 
+func readFile(repo *git.Repository, name string) (string, error){
+	fileName := filepath.Join(repo.Path, name)
+	bytes, err := ioutil.ReadFile(fileName)
+	return string(bytes), err
+}
+
 // Basic merge of 3 branches. Asserts the resulting tree and the merge commit
 func TestOctopus3branches(t *testing.T) {
 	context, _ := CreateTestContext()
@@ -83,4 +89,37 @@ func TestOctopus3branches(t *testing.T) {
 			"refs/heads/branch2\n"+
 			"refs/heads/branch3\n"+
 			"\nCommit created by git-octopus "+VERSION+".")
+}
+
+func TestOctopusDuplicateParent(t *testing.T) {
+	context, outputBuffer := CreateTestContext()
+	repo := context.Repo
+	defer test.Cleanup(repo)
+
+	// Commit a new file on master
+	writeFile(repo, "testFile", "line1")
+	repo.Git("add", "testFile")
+	repo.Git("commit", "-m", "Add first file")
+
+	// Create an empty branch
+	repo.Git("branch", "feature1")
+
+	// Create a 2nd feature branch
+	repo.Git("checkout", "-b", "feature2")
+	// Edit the previously created file
+	writeFile(repo, "testFile", "line1", "line2")
+	repo.Git("add", "testFile")
+	repo.Git("commit", "-m", "Add line2")
+
+	// Run octopus from master
+	repo.Git("checkout", "master")
+	err := Run(context, "feature*")
+	assert.Nil(t, err)
+
+	// The file should be in the state of feature2 branch
+	testFile, _ := readFile(repo, "testFile")
+	assert.Equal(t, "line1\nline2", testFile)
+
+	// The test case should reproduced the duplace parent error
+	assert.Contains(t, outputBuffer.String(), "duplicate parent")
 }
